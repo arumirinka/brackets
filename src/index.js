eabe8e4042ec0494a0ac8275ec3232691df42244
Irina Stepanova <irina.s.vesna@mail.ru>
2020-10-19 02:39:50 +0600

    feat: add brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = [str.charAt(0)];
+  for (let i=1; i<str.length; i++) {
+    res.push(str.charAt(i));
+    bracketsConfig.forEach(element => {
+      if (element.join('') === [res[res.length - 2], res[res.length - 1]].join('')) {
+        res.pop();
+        res.pop();
+      }
+    });
+  }
+  return res.length === 0;
 }
